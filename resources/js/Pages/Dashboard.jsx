95b95fe8e790ea0d525ccb0b0978b5103299002e
pradiknatama <pradiknatama@gmail.com>
2025-01-11 09:00:22 +0700

    refactoring load data dashboard

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -15,15 +15,6 @@ export default function Dashboard({ auth,total_tarif_rs }) {
                 <div className="row">
                     <div className="col-sm-4">
                         <div className="card text-white bg-primary mb-3">
-                            <div className="card-header"><center>Total Tarif RS</center></div>
-                            <div className="card-body">
-                                <p className="card-text"><center>{total_tarif_rs.total_tarif_rs}</center></p>
-                                
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-sm-4">
-                        <div className="card text-white bg-primary mb-3">
                             <div className="card-header"><center>Jumlah Claim</center></div>
                             <div className="card-body">
                                 <p className="card-text"><center>{total_tarif_rs.count_total}</center></p>
@@ -36,6 +27,15 @@ export default function Dashboard({ auth,total_tarif_rs }) {
                             <div className="card-header"><center>Total Tarif RS</center></div>
                             <div className="card-body">
                                 <p className="card-text"><center>{total_tarif_rs.total_tarif_rs}</center></p>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-4">
+                        <div className="card text-white bg-primary mb-3">
+                            <div className="card-header"><center>Total Tarif INA-CBG </center></div>
+                            <div className="card-body">
+                                <p className="card-text"><center>{total_tarif_rs.tarifgruper}</center></p>
                                 
                             </div>
                         </div>
