f253bc743adbcd29369d9e11d513ce84678f8b69
pradiknatama <pradiknatama@gmail.com>
2025-02-08 15:26:04 +0700

    bugFix new claim

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -57,7 +57,7 @@ class LoginController extends Controller
                 $request->session()->regenerate();
 
                 // return redirect()->intended('/dashboard');
-                return redirect()->route('/dashboard');
+                return redirect()->route('dashboard');
             }
         }
 
